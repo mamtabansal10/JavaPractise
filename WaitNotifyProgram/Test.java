2f52fa4cc2dcf750c92dbd82069398c1c1a2de51
mamtabansal10 <42738717+mamtabansal10@users.noreply.github.com>
2022-05-10 09:37:08 +0530

    Update Test.java

--- WaitNotifyProgram/Test.java
+++ WaitNotifyProgram/Test.java
@@ -1,1 +1,30 @@
 
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+		ImplementWaitNotifyMethods obj = new ImplementWaitNotifyMethods();
+
+		// Thread t1 will call wait()
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					obj.callingWait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// Thread t2 will call notify()
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				obj.callingNotify();
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+	}
+}
