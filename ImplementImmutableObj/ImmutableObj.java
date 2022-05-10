1ddae8a71089a90333838c6c07313764021b5943
mamtabansal10 <42738717+mamtabansal10@users.noreply.github.com>
2022-05-10 09:30:58 +0530

    Update ImmutableObj.java

--- ImplementImmutableObj/ImmutableObj.java
+++ ImplementImmutableObj/ImmutableObj.java
@@ -1,1 +1,27 @@
+package com.java.practise;
+
+public final class ImmutableObj {
+	
+	public final int i;
+	
+	public ImmutableObj(int i) {
+		this.i = i;
+	}
+    
+	public int getValue() {
+		return i;
+	}
+	
+	public ImmutableObj modify(int i) {
+		if (i==this.i) {
+			return this;
+		}
+		else {
+		return new ImmutableObj(i);}
+		
+	}
+	
+	
+}
+
 
