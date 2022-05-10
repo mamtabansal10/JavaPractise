e6188913790ae8103e960c68b4399cb5528b395f
mamtabansal10 <42738717+mamtabansal10@users.noreply.github.com>
2022-05-10 09:30:17 +0530

    Add files via upload

---
+++ ImplementImmutableObj/ImmutableRunner.java
@@ -0,0 +1,17 @@
+package com.java.practise;
+
+public class ImmutableRunner {
+	
+		public static void main (String[] args) {
+			ImmutableObj obj  = new ImmutableObj(10);
+			System.out.println(obj.getValue());
+			ImmutableObj obj2 = obj.modify(10);
+			System.out.println(obj2.getValue());
+			System.out.println(obj==obj2);
+			ImmutableObj obj3 = new ImmutableObj(12);
+			System.out.println(obj3.getValue());
+			System.out.println(obj==obj3);
+			
+		}
+
+}
