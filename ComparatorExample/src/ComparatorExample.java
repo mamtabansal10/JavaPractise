155ac80553351a67ddb596520133b0c1356fa801
mamtabansal10 <42738717+mamtabansal10@users.noreply.github.com>
2022-05-10 09:23:57 +0530

    Update ComparatorExample.java

--- ComparatorExample/src/ComparatorExample.java
+++ ComparatorExample/src/ComparatorExample.java
@@ -1,1 +1,15 @@
 
+
+
+import java.util.Comparator;
+
+public class ComparatorExample implements Comparator<Object> {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		String s1 = o1.toString();
+		String s2 = o2.toString();
+		return s2.compareTo(s1);
+	}
+
+}
