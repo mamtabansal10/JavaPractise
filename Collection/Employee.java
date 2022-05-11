9587fb9b8ff1a86b643723ca72de64b9c69805b0
mamtabansal10 <42738717+mamtabansal10@users.noreply.github.com>
2022-05-11 11:24:29 +0530

    Add files via upload

---
+++ Collection/Employee.java
@@ -0,0 +1,51 @@
+
+
+public class Employee {
+
+	private int id;
+	
+	// override hashcode and equals methods to implement logic for Employee object key comaprison
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		return (this.id == ((Employee) obj).id);
+
+	}
+
+	private String name;
+
+	public Employee(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
