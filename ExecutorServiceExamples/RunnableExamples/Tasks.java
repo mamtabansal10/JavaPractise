9d48c4d1be5fe52ed7ce54157edafaf9e33d905a
mamtabansal10 <42738717+mamtabansal10@users.noreply.github.com>
2022-05-17 11:21:13 +0530

    Add files via upload

---
+++ ExecutorServiceExamples/RunnableExamples/Tasks.java
@@ -0,0 +1,17 @@
+package com.executor.service;
+
+public class Tasks extends Thread {
+
+	public int a = 0;
+
+	@Override
+	public void run() {
+        System.out.println("Task Started ");
+        System.out.println(" ");
+		for (int i = 0; i <= 5; i++) {
+			System.out.print(" " + i);
+		}
+        System.out.println(" Task Done");
+	}
+
+}
